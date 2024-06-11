28bdef38d280298ea5217be3dcfcb9c4a5f3c7e8
odairjgn <odairjgn@outlook.com>
2024-06-11 10:49:30 -0300

    rf

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useRef, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Webcam from "react-webcam";
+
+type Modo = "fotos" | "video";
 
 function App() {
+  const [inverter, setInverter] = useState(false);
+  const webcamRef = useRef<Webcam>(null);
+
+  const configs: MediaTrackConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user",
+  };
+
+  const handleCapturar = useCallback(() => {
+    const foto = webcamRef.current?.getScreenshot();
+    downloadBase64Jpg(foto!);
+  }, [webcamRef]);
+
+  function downloadBase64Jpg(contentBase64: string) {
+    const downloadLink = document.createElement("a");
+    document.body.appendChild(downloadLink);
+
+    downloadLink.href = contentBase64;
+    downloadLink.target = "_self";
+    downloadLink.download = new Date().toISOString() + ".jpg";
+    downloadLink.click();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div>
+          <h2>Captura de foto</h2>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            name="inverter"
+            checked={inverter}
+            onChange={(e) => setInverter(e.target.checked)}
+          />
+          <label htmlFor="inverter">Inverter</label>
+        </div>
+        <div className="camera">
+          <Webcam
+            ref={webcamRef}
+            height={360}
+            screenshotFormat="image/jpeg"
+            videoConstraints={configs}
+            mirrored={inverter}
+          />
+        </div>
+        <button onClick={handleCapturar}>Capturar</button>
+      </div>
     </div>
   );
 }
